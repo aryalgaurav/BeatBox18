0663eab9fc33ac8b011e6eb82a79d7504d2b1513
aryalg1@nku.edu <aryalg1@nku.edu>
2016-09-10 14:47:26 -0400

    Starting Chapter 18.

---
+++ app/src/main/java/android/nku/edu/beatbox18/SingleFragmentActivity.java
@@ -0,0 +1,10 @@
+package android.nku.edu.beatbox18;
+
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+
+public abstract class SingleFragmentActivity extends AppCompatActivity {
+    protected abstract Fragment createFragment();
+
+
+}
